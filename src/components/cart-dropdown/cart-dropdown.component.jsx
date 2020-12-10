93a9e7337ab49e5d9bc0f6ea896a4a97c51ee3fc
unknown <vikichet@gmail.com>
2020-12-10 10:53:50 +0530

    updated with memorise selectors +in redux

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import { connect } from 'react-redux'
+
 import CustomButton from '../custom-button/custom-button.component';
+
+import CartItem from '../cart-item/cart-item.component';
+
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.style.scss'
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className="cart-items">
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+            {
+                cartItems.map(cartItem =>
+                    (<CartItem key={cartItem.id} item={cartItem} />)
+                )
+            }
         </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({ cartItems: selectCartItems(state) });
+
+export default connect(mapStateToProps)(CartDropdown);
