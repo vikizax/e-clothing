200edc393a9f611f9fdb72737944ee41044a5945
unknown <vikichet@gmail.com>
2021-05-23 19:49:44 +0530

    separated client and server, add express server for stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
-
 import StripeCheckout from 'react-stripe-checkout'
-
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51I0qxCJ1UdtQYk7MZPuNnW4XOGVJ1jTa9GyRlL3Dtt9em8ay8KHHv6a3ipf8U8ukK4xCw1PLOv5MJrhmimQf3ann00Xu0gLbwC';
+
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+        // console.log(token);
+        // alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then((res) => {
+            alert('Payment Successfull');
+        }).catch(error => {
+            console.log('Payment error:', error);
+            alert(
+                'There was an issur with your payment. Please try again.'
+            );
+        })
     }
+
     return (
         <StripeCheckout
             label='Pay Now'
             name='EClothing Ltd.'
             billingAddress
             shippingAddress
-            image='https://sendeyo.com/up/d/f3eb2117da'
+            // image='https://sendeyo.com/up/d/f3eb2117da'
             description={`Your total is $${price}`}
             amount={priceForStripe}
             panelLabel='Pay Now'
